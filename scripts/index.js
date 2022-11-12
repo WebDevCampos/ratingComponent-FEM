39571c25c886f3fab533f5434472088ad7d1c7ac
Marcus Rangel <webdevcmpos@gmail.com>
2022-11-12 12:45:07 -0300

    Almost there

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const innerCard = document.querySelector(".inner-card");
+const value = document.querySelectorAll('input[type="radio"]');
+const form = document.querySelector("form");
+const finalRating = document.querySelector(".final-rating");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(value[0].validity, value);
+  value.forEach((item) => {
+    const valueIsSelected = item.value == e.target.rating.value;
+    if (valueIsSelected) {
+      finalRating.innerText = item.value;
+      innerCard.style.transform = "rotateY(180deg)";
+    }
+  });
+});
